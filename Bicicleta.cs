dc4085270192cccb5dce53dfb348915c66f6ef7a
SusanaRG <53104721+SusanaRG@users.noreply.github.com>
2020-02-04 11:37:02 -0500

    Add files via upload

---
+++ Bicicleta.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicicleta.Clases
+{
+    class Bicicleta
+    {
+        //Atributos
+        public string Marca;
+        public string Color;
+        public string Material;
+        public float TamanoMarco = 0F;
+        public float TamanoLlanta = 0F;
+        public short NumeroCambios;
+        public short CambioActual;
+        public short VelocidadActual;
+        public short VelocidadMaxima;
+
+        //Constructor
+        public Bicicleta()
+        {
+            try
+            {
+
+            }
+            catch(Exception Error)
+            {
+
+            }
+        }
+
+        //Metodos
+        public void Acelerar(short Vel_Objetivo)
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+
+        public void Desacelerar(short Vel_Objetivo)
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+
+        public void SubirCambio()
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+
+        public void SubirCambio(short CambioFinal)
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+
+        public void BajarCambio()
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+
+        public void BajarCambio(short CambioFinal)
+        {
+            try
+            {
+
+            }
+            catch (Exception Error)
+            {
+
+            }
+        }
+    }
+}
