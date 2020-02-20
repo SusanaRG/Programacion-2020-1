2ec69df84230c0e6ec1a02315e09e7854c8ea5e4
SusanaRG <53104721+SusanaRG@users.noreply.github.com>
2020-02-20 11:54:02 -0500

    Add files via upload

---
+++ Extracto.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extracto_de_puntos.Clases
+{
+    class Extracto
+    {
+       private string nombre_cliente;
+       private UInt32 nro_ID;
+       private UInt64 telefono;
+       private UInt64 saldo_puntos;
+
+        public string Nombre_cliente
+        {
+         get { return nombre_cliente.ToUpper(); }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value) || value.Length < 5) throw new Exception("El nombre no cumple los requisitos");
+                else nombre_cliente = value.ToUpper();
+            }
+         }
+        public uint Nro_ID
+        {
+         get { return nro_ID; }
+         set
+         {
+                if (value > 0)
+                {
+                    nro_ID = value;
+                }
+                else throw new Exception("Numero del ID no cumple");
+                
+         }
+        }
+
+
+        public ulong Telefono
+        {
+         get { return telefono; }
+         set
+            {
+                if (value >= 1000000 && value <= 9999999999)
+                {
+                    telefono = value;
+                }
+                else throw new Exception("Numero de telefono invlido");
+            }
+        }
+        public ulong Saldo_puntos  { get => saldo_puntos; }
+
+        public Extracto(string nombre_cliente, uint nro_ID, ulong telefono)
+        {
+            try
+            {
+                this.nombre_cliente = nombre_cliente;
+                this.nro_ID = nro_ID;
+                this.telefono = telefono;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Problema al crear el objeto");
+            }
+        }
+
+        public ulong Acumular_Puntos(ulong Vlr_Compra)
+        {
+            ulong Vlr_Acumulado = 0;
+            try
+            { 
+              //if(Vlr_Compra >= 100)
+               Vlr_Acumulado = Vlr_Compra / 100;             
+                return Vlr_Acumulado;
+            }
+            catch (Exception)
+            {
+                return Vlr_Acumulado;
+            }
+        }
+
+        public bool Redimir_Puntos(ulong Puntos)
+        {
+            
+            try
+            {
+                if(saldo_puntos >= Puntos)
+                {
+                    saldo_puntos -= Puntos;
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProgramExtractoP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extracto_de_puntos
+{
+    class Program
+    {
+        static void Main()
+        {
+           Console.WriteLine(Puntos_Articulo());
+        }
+
+       public static uint Puntos_Articulo()
+        {
+            uint Puntos = 0;
+            short Opcion = 0;
+            try
+            {
+                do
+                {
+                    Console.WriteLine("1. Artículo de decoración ==> 5.000 Puntos ");
+                    Console.WriteLine("2. Artículo Electrónico menor ==> 10.000 Puntos ");
+                    Console.WriteLine("3. Artículo Electrónico mediano ==> 50.000 Puntos ");
+                    Console.WriteLine("4. Tiquete nacional un trayecto ==> 90.000 Puntos ");
+                    Console.WriteLine("5. Artículo Electrónico grande ==> 150.000 Puntos ");
+                    Console.WriteLine("6. Tiquete nacional RoundTrip ==> 180.000 Puntos ");
+                    Console.WriteLine("Escriba su opción");
+
+                    try
+                    {
+                        Opcion = short.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        Opcion = 0;
+                    }
+
+                } while (Opcion < 1 && Opcion < 6);
+
+                return Puntos;
+            }
+            catch (Exception)
+            {
+                return Puntos;
+            }
+           
+        }
+
+
+    }
+}
